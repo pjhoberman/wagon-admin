6c5e9294c8ffcf0ff10f4b37e458feeda3d4020a
PJ Hoberman <pj.hoberman@gmail.com>
2013-04-05 22:39:14 -0600

    lead admin

--- wagonadmin/leads/models.py
+++ wagonadmin/leads/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Lead(models.Model):
+    summary = models.CharField(max_length=140, help_text="Describe it in a tweet or less")
+    description = models.TextField(null=True, blank=True, help_text="More details go here")
+    date_entered = models.DateField(auto_now_add=True)
+    date_due = models.DateField(blank=True, null=True)
+    entered_by = models.ForeignKey('auth.User', related_name="lead_entered_by")
+    assigned_to = models.ForeignKey('auth.User', related_name="lead_assigned_to")
